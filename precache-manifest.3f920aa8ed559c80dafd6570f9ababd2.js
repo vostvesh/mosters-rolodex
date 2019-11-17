ed701c9f70363119479b426ee736f789d67f3451
Aliaksandr Shautsou <aliaksandr.shautsou@tele2.com>
2019-11-17 19:52:37 +0300

    Updates

---
+++ precache-manifest.3f920aa8ed559c80dafd6570f9ababd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c21ccfddf80490a49e61a21e00e458a6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b035968e739c65e24fe5",
+    "url": "/monsters-rolodex/static/css/main.91cfd8e9.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "b035968e739c65e24fe5",
+    "url": "/monsters-rolodex/static/js/main.9729fe35.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
